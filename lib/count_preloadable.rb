d9ae68def46d5e86f3f1e743482dfe3ab6146570
Takashi Kokubun <takashikkbn@gmail.com>
2014-09-12 14:54:23 +0900

    Override ActiveRecord::Associations.has_many

--- lib/count_preloadable.rb
+++ lib/count_preloadable.rb
@@ -1,5 +1,8 @@
-require "count_preloadable/version"
-
-module CountPreloadable
-  # Your code goes here...
+module ActiveRecord::Associations
+  module ClassMethods
+    def has_many_with_count_preloadable(*args)
+      has_many_without_count_preloadable(*args)
+    end
+    alias_method_chain :has_many, :count_preloadable
+  end
 end
