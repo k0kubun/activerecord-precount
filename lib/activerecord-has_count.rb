92c1a6f39cee8e1b3d8fb1f4af669681f485cc47
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-22 21:07:54 +0900

    Move model file

--- lib/activerecord-has_count.rb
+++ lib/activerecord-has_count.rb
@@ -8,7 +8,7 @@ require "active_record/associations/join_dependency/has_count"
 require "active_record/reflection/has_count"
 
 ActiveSupport.on_load(:active_record) do
-  require "activerecord-has_count/model"
+  require "active_record/has_count/model"
   ActiveRecord::Base.send(:include, ActiveRecord::HasCount::Model)
 end
 
