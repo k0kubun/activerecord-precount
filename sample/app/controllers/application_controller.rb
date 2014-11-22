3fd5bfa0929d36cfc9216ee53d821661752ed0e7
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-22 14:34:54 +0900

    Show benchmark text

--- sample/app/controllers/application_controller.rb
+++ sample/app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   def index
     @tweets = Tweet.all
+    if eager_load?
+      @tweets = @tweets.preload(:replies_count, tweet: :favorites_count)
+    end
   end
+
+  def eager_load?
+    params[:eager_load] == 'true'
+  end
+  helper_method :eager_load?
 end
