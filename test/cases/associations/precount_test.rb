8c4b9eb6b048fc0bd167051ec8ed2176146d5779
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-01 14:40:46 +0900

    Fix precount test

--- test/cases/associations/precount_test.rb
+++ test/cases/associations/precount_test.rb
@@ -6,6 +6,14 @@ class PrecountTest < ActiveRecord::CountLoader::TestCase
       tweet = Tweet.create
       index.times { Favorite.create(tweet: tweet) }
     end
+
+    if Tweet.has_reflection?(:favs_count)
+      if ActiveRecord::VERSION::MAJOR >= 4 && ActiveRecord::VERSION::MINOR >= 2
+        Tweet._reflections.delete('favs_count')
+      else
+        Tweet._reflections.delete(:favs_count)
+      end
+    end
   end
 
   def teardown
@@ -16,11 +24,10 @@ class PrecountTest < ActiveRecord::CountLoader::TestCase
     3
   end
 
-  def test_precount_has_many_does_not_execute_n_1_queries
-    assert_equal(Tweet.reflections['favs_count'].present?, false)
-    assert_queries(1 + tweets_count) { Tweet.all.map { |t| t.favs.count } }
-    assert_queries(2) { Tweet.precount(:favs).map { |t| t.favs.count } }
-    assert_queries(2) { Tweet.precount(:favs).map(&:favs_count) }
+  def test_precount_defines_count_loader
+    assert_equal(Tweet.has_reflection?(:favs_count), false)
+    Tweet.precount(:favs).map(&:favs_count)
+    assert_equal(Tweet.has_reflection?(:favs_count), true)
   end
 
   def test_precount_has_many_with_count_loader_does_not_execute_n_1_queries
@@ -33,6 +40,7 @@ class PrecountTest < ActiveRecord::CountLoader::TestCase
   def test_precount_has_many_counts_properly
     expected = Tweet.all.map { |t| t.favorites.count }
     assert_equal(Tweet.all.map(&:favorites_count), expected)
+    assert_equal(Tweet.precount(:favorites).map { |t| t.favorites.count }, expected)
     assert_equal(Tweet.precount(:favorites).map(&:favorites_count), expected)
   end
 end
