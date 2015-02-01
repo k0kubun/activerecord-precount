1e7a8c1e02a5be6cdb4b6d3cf328cff789cb3afe
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-01 16:50:11 +0900

    Use eager_count in sample app

--- sample/app/controllers/application_controller.rb
+++ sample/app/controllers/application_controller.rb
@@ -1,13 +1,33 @@
 class ApplicationController < ActionController::Base
+  AVAILABLE_LOADERS = %w[N+1 precount eager_count].freeze
+  DEFAULT_LOADER    = 'N+1'
+
   def index
     @tweets = Tweet.all
-    if eager_load?
+    case loader
+    when 'precount'
       @tweets = @tweets.precount(:replies).preload(in_reply_to: :favorites_count)
+    when 'eager_count'
+      @tweets = @tweets.eager_count(:replies).eager_load(in_reply_to: :favorites_count)
     end
   end
 
-  def eager_load?
-    params[:eager_load] == 'true'
+  def precount?
+    loader == 'precount'
   end
-  helper_method :eager_load?
+  helper_method :precount?
+
+  def eager_count?
+    loader == 'eager_count'
+  end
+  helper_method :eager_count?
+
+  def loader
+    if AVAILABLE_LOADERS.include?(params[:loader])
+      params[:loader]
+    else
+      DEFAULT_LOADER
+    end
+  end
+  helper_method :loader
 end
