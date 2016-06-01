096a83d4a31b175c0fbd76872f39b64dd67a7859
Takashi Kokubun <takashikkbn@gmail.com>
2016-06-02 01:03:17 +0900

    Resolve deprecation warnings toward Rails 5

--- test/schema/schema.rb
+++ test/schema/schema.rb
@@ -2,7 +2,7 @@ ActiveRecord::Schema.define do
   create_table :favorites, force: true do |t|
     t.integer  :tweet_id
     t.integer  :user_id
-    t.timestamps
+    t.timestamps null: false
   end
   add_index :favorites, :tweet_id
 
@@ -10,7 +10,7 @@ ActiveRecord::Schema.define do
     t.integer  :in_reply_to_tweet_id
     t.integer  :user_id
     t.integer  :favorites_count_cache
-    t.timestamps
+    t.timestamps null: false
   end
   add_index :tweets, :in_reply_to_tweet_id
 end
