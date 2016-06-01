abf452b4e8941a8cd79bd9b2d613a9d1e555ec28
Takashi Kokubun <takashikkbn@gmail.com>
2016-06-02 02:01:38 +0900

    Drop extend.rb

--- lib/active_record/precount.rb
+++ lib/active_record/precount.rb
@@ -1,4 +1,23 @@
 require "active_record"
 require "active_support/lazy_load_hooks"
 
-require "active_record/precount/extend"
+require "active_record/precount/base_extension"
+require "active_record/precount/collection_proxy_extension"
+require "active_record/precount/has_many_extension"
+require "active_record/precount/join_dependency_extension"
+require "active_record/precount/preloader_extension"
+require "active_record/precount/reflection_extension"
+require "active_record/precount/relation_extension"
+
+ActiveSupport.on_load(:active_record) do
+  module ActiveRecord
+    Base.send(:extend, Precount::BaseExtension)
+    Relation.prepend(Precount::RelationExtension)
+    Reflection.prepend(Precount::ReflectionExtension)
+    Associations::Preloader.prepend(Precount::PreloaderExtension)
+    Associations::JoinDependency.prepend(Precount::JoinDependencyExtension)
+    Associations::CollectionProxy.prepend(Precount::CollectionProxyExtension)
+    Associations::Builder::HasMany.prepend(Precount::Builder::HasManyExtension)
+    Reflection::AssociationReflection.prepend(Precount::AssociationReflectionExtension)
+  end
+end
