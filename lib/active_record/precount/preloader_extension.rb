984b71174b5ceb1704f15d5e3bbb6a3c6e2cd393
Takashi Kokubun <takashikkbn@gmail.com>
2016-06-03 03:07:18 +0900

    Refactor preloader

--- lib/active_record/precount/preloader_extension.rb
+++ lib/active_record/precount/preloader_extension.rb
@@ -2,23 +2,27 @@ module ActiveRecord
   module Associations
     class Preloader
       class CountLoader < SingularAssociation
+        def association_key_name
+          reflection.foreign_key
+        end
+
+        def owner_key_name
+          reflection.active_record_primary_key
+        end
+
+        private
+
+        def preload(preloader)
+          associated_records_by_owner(preloader).each do |owner, associated_records|
+            owner.association(reflection.name).target = associated_records.first.to_i
+          end
+        end
+
+        def query_scope(ids)
+          scope.where(association_key.in(ids)).group(association_key_name).count(association_key_name)
+        end
+
         if ActiveRecord.version.segments.first >= 5
-          def association_key_name
-            reflection.foreign_key
-          end
-
-          def owner_key_name
-            reflection.active_record_primary_key
-          end
-
-          private
-
-          def preload(preloader)
-            associated_records_by_owner(preloader).each do |owner, associated_records|
-              owner.association(reflection.name).target = associated_records.first.to_i
-            end
-          end
-
           def load_records
             return {} if owner_keys.empty?
 
@@ -29,27 +33,7 @@ module ActiveRecord
 
             Hash[@preloaded_records.first.map { |key, count| [key, [count]] }]
           end
-
-          def query_scope(ids)
-            scope.where(association_key.in(ids)).group(association_key_name).count(association_key_name)
-          end
         else
-          def association_key_name
-            reflection.foreign_key
-          end
-
-          def owner_key_name
-            reflection.active_record_primary_key
-          end
-
-          private
-
-          def preload(preloader)
-            associated_records_by_owner(preloader).each do |owner, associated_records|
-              owner.association(reflection.name).target = associated_records.first.to_i
-            end
-          end
-
           def load_slices(slices)
             @preloaded_records = slices.flat_map { |slice|
               records_for(slice)
@@ -58,10 +42,6 @@ module ActiveRecord
             @preloaded_records.first.map { |key, count|
               [count, key]
             }
-          end
-
-          def query_scope(ids)
-            scope.where(association_key.in(ids)).group(association_key_name).count(association_key_name)
           end
         end
       end
